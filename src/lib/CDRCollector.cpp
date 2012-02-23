fd777a4e2432df9ea4aaa54f929523b313b0c250
Fridrich Štrba <fridrich.strba@bluewin.ch>
2012-02-23 11:34:33 +0100

    tabs -> spaces

--- src/lib/CDRCollector.cpp
+++ src/lib/CDRCollector.cpp
@@ -586,9 +586,9 @@ void libcdr::CDRCollector::_lineProperties(WPXPropertyList &propList)
         propList.insert("draw:stroke", "dash");
       else
         propList.insert("draw:stroke", "solid");
-	  if (iter->second.lineType & 0x20) // hairline flag ???
-	    propList.insert("svg:stroke-width", 0.0);
-	  else
+      if (iter->second.lineType & 0x30) // hairline flag ???
+        propList.insert("svg:stroke-width", 0.0);
+      else
         propList.insert("svg:stroke-width", iter->second.lineWidth);
       propList.insert("svg:stroke-color", _getRGBColorString(iter->second.colorModel, iter->second.color));
 
