b6a295e599f93f6ff6fcda489c1c34479c846728
David Tardon <dtardon@redhat.com>
2015-08-17 17:05:27 +0200

    impl. seek-to-end in internal stream
    Change-Id: Id2401fd09bd2d4a5c54caeb2502932969d9e61f8

--- src/lib/CDRInternalStream.cpp
+++ src/lib/CDRInternalStream.cpp
@@ -126,6 +126,8 @@ int libcdr::CDRInternalStream::seek(long offset, librevenge::RVNG_SEEK_TYPE seek
     m_offset += offset;
   else if (seekType == librevenge::RVNG_SEEK_SET)
     m_offset = offset;
+  else if (seekType == librevenge::RVNG_SEEK_END)
+    m_offset = long(static_cast<unsigned long>(m_buffer.size())) + offset;
 
   if (m_offset < 0)
   {
