18874143a46c479cbb82341d7aa6f29504db4725
David Tardon <dtardon@redhat.com>
2017-04-15 17:35:07 +0200

    workaround difference in astyle versions
    Change-Id: I6c517ba4956372538015712c3446818e3c443b88

--- src/lib/CDROutputElementList.cpp
+++ src/lib/CDROutputElementList.cpp
@@ -407,7 +407,7 @@ libcdr::CDROutputElementList::CDROutputElementList(const libcdr::CDROutputElemen
 libcdr::CDROutputElementList &libcdr::CDROutputElementList::operator=(const libcdr::CDROutputElementList &elementList)
 {
   for (std::vector<CDROutputElement *>::iterator iter = m_elements.begin(); iter != m_elements.end(); ++iter)
-    delete (*iter);
+    delete *iter;
 
   m_elements.clear();
 
@@ -420,7 +420,7 @@ libcdr::CDROutputElementList &libcdr::CDROutputElementList::operator=(const libc
 libcdr::CDROutputElementList::~CDROutputElementList()
 {
   for (std::vector<CDROutputElement *>::iterator iter = m_elements.begin(); iter != m_elements.end(); ++iter)
-    delete (*iter);
+    delete *iter;
   m_elements.clear();
 }
 
